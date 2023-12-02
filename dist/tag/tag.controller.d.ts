b32ec82ab6c3213d63a3b4b29b5a6bb6de865fd2
Nazarii <nazzikk1998@gmail.com>
2023-12-02 09:16:44 +0200

    removed node_modules and dist

--- dist/tag/tag.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { TagService } from "./tag.service";
-export declare class TagController {
-    private readonly tagService;
-    constructor(tagService: TagService);
-    findAll(): string[];
-}
--- dist/tag/tag.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class TagService {
-    findAll(): string[];
-}
