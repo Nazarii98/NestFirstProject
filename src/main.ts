a4a18f88df3df7d05d89b077072d6956ac6668ea
Nazarii <nazzikk1998@gmail.com>
2023-12-03 08:53:10 +0200

    implemented paths for development and deplouyment

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+if(!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
+
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
